4a248386b6b60fcaeef8bd8af260c538c617727e
Devin Herath <devdhera@gmail.com>
2019-01-03 00:29:26 +0530

    Added Tests

---
+++ src/test/java/com/devin/filerest/FileRestApplicationTests.java
@@ -0,0 +1,17 @@
+package com.devin.filerest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FileRestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
